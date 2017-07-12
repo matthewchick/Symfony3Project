a5a12898227d8649468ef8faf8fddb3adf33612f
Matthew Chick <chikmatthew@gmail.com>
2017-07-12 16:16:25 +1000

    How to pass an array from controller to the view (template) and how to use for loop statement in twig

--- src/CarBundle/Controller/DefaultController.php
+++ src/CarBundle/Controller/DefaultController.php
@@ -12,6 +12,18 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('CarBundle:Default:index.html.twig');
+        $cars = [
+            ['make'=> 'BMW', 'name' => 'X1'],
+            ['make'=> 'Honda', 'name' => 'Jazz'],
+            ['make'=> 'Audi', 'name' => 'Q7'],
+        ];
+        /* http://symfony.com/doc/current/controller.html
+           A controller is a PHP function you create that reads information from the Symfony's Request object and creates and returns a Response object.
+           The response could be an HTML page, JSON, XML, a file download, a redirect, a 404 error or anything else you can dream up.
+           The controller executes whatever arbitrary logic your application needs to render the content of a page.
+           The render() method renders a template and puts that content into a Response object
+           return $this->render('CarBundle:Default:index.html.twig', array('cars' => $cars));
+        */
+        return $this->render('CarBundle:Default:index.html.twig', ['cars' => $cars]);
     }
 }
